85afd7a46ed6319ffb472e5344b6fe809d20616c
jayanta <jayantamahato830@gmail.com>
2022-10-27 20:17:42 +0530

    improved user interface!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
+
 import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import Todo from './components/todo';
 
--- frontend/src/components/todo.tsx
+++ frontend/src/components/todo.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+
+
 import './css/todo.css';
+import swal from 'sweetalert';
+
 import { useState } from "react";
 import { useEffect } from 'react';
 
@@ -7,10 +17,11 @@ import { addTask } from '../services/api';
 import { fetchList } from '../services/api';
 import { finishTask } from '../services/api';
 
+
 const Todo = () => {
 
     const [list, setList] = useState([]);
-    const [task,setTask] = useState([]);
+    const [task, setTask] = useState([]);
 
     useEffect(() => {
         fetchListFunction();
@@ -27,12 +38,21 @@ const Todo = () => {
         e.preventDefault();
         const jsonlist = JSON.stringify(list);
         const result = await addTask(list);
+        if(result){
+            swal("Great!", "Your Task Added!", "success");
+        }else{
+            swal("Error", "We Detect Some Error!", "Error");
+        }
         fetchListFunction();
     }
 
-    const completed = async (id:any)=>{
+    const completed = async (id: any) => {
         const result = await finishTask(id);
-        console.log(result.status);
+        if(result){
+            swal("Great!", "You Completed One Task!", "success");
+        }else{
+            swal("Error", "We Detect Some Error!", "Error");
+        }
         fetchListFunction();
     }
 
@@ -41,43 +61,58 @@ const Todo = () => {
             <h5 className="heading"> to do list</h5>
             <div className="add-form">
                 <form onSubmit={(e) => handleSubmit(e)} >
-                    <textarea name="task" onChange={(e) => { setList({ ...list, [e.target.name]: e.target.value }) }} required/>
-                    <select name="priority" onChange={(e) => { setList({ ...list, [e.target.name]: e.target.value }) }}  required>
-                        
-                        <option value="null" selected disabled> SET PRIORITY</option>
-                        <option value="high">HIGH</option>
-                       
-                        <option value="low">LOW</option>
-                    </select>
-                    <input type="date" name="date" onChange={(e) => { setList({ ...list, [e.target.name]: e.target.value }) }} required/>
+                    <TextField id="outlined-basic" label="Task" variant="outlined" name="task" onChange={(e) => { setList({ ...list, [e.target.name]: e.target.value }) }} required />
+                    <div>
+                        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                            <InputLabel id="demo-simple-select-standard-label">Priority</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-standard-label"
+                                id="demo-simple-select-standard"
+                                label="Priority"
+                                name="priority"
+                                onChange={(e) => { setList({ ...list, [e.target.name]: e.target.value }) }}
+                                required
+                            >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                                <MenuItem value="high">High</MenuItem>
+                                <MenuItem value="low">Low</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
 
-                    <button type="submit"> add task</button>
+
+                    {/* <input type="date" name="date" onChange={(e) => { setList({ ...list, [e.target.name]: e.target.value }) }} required /> */}
+                    <TextField id="outlined-basic" label="" variant="outlined" type="date" name="date" onChange={(e) => { setList({ ...list, [e.target.name]: e.target.value }) }} required />
+
+                    <button type="submit"> Add Task</button>
                 </form>
             </div>
             <div className="list">
                 <table>
                     <thead>
-                    <tr>
-                        <th>priority</th>
-                        <th>task</th>
-                        <th>date</th>
-                        <th>completed</th>
-                    </tr>
+                        <tr>
+                            <th>priority</th>
+                            <th>task</th>
+                            <th>date</th>
+                            <th>Status</th>
+                        </tr>
                     </thead>
-                    
+
                     <tbody>
-                    {
-                        task.map((task,value) => (
-                            <tr key={value}>
-                                <td>{task.priority}</td>
-                                <td>{task.task}</td>
-                                <td>{task.date}</td>
-                                <td><button onClick={()=>completed(task._id)}>done</button></td>
-                            </tr>
-                        ))
-                    }
+                        {
+                            task.map((task, value) => (
+                                <tr key={value}>
+                                    <td>{task.priority}</td>
+                                    <td>{task.task}</td>
+                                    <td>{task.date}</td>
+                                    <td><button onClick={() => completed(task._id)}>Completed</button></td>
+                                </tr>
+                            ))
+                        }
                     </tbody>
-                    
+
 
                 </table>
             </div>
